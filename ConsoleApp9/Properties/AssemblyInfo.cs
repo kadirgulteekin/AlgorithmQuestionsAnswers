0a4ffd262dc35eb07ff9dc9c9c0c5f7445570673
kadirgulteekin <kadirgulteekin@gmail.com>
2023-05-01 13:18:18 +0300

    name changed.

--- ConsoleApp9/Properties/AssemblyInfo.cs
+++ ConsoleApp9/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp9")]
+[assembly: AssemblyTitle("IsPalindrome")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp9")]
+[assembly: AssemblyProduct("IsPalindrome")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
